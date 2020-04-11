505fbeba6fac0d4dbc60e8f1dacdb033c3680a19
bmusuko <bram.musuko@gmail.com>
2020-04-11 11:02:22 +0700

    add quote feature

--- index.js
+++ index.js
@@ -44,3 +44,46 @@ bot.onText(/\/random/, (msg, match) => {
 
 });
 
+
+bot.onText(/\/jennie/, (msg, match) => {
+	let chatId = msg.chat.id;
+	console.log(msg);
+	// axios.get('https://picsum.photos//500/500.jpg/?blur=3'){
+		
+	// }
+	bot.sendPhoto(chatId,`./images/jennie/f01b6074d3b3abb74e5d8e549b92d1bb.jpg`); 
+
+});
+
+bot.onText(/\/quote (.*)/, (msg, match) => {
+	console.log("hit")
+	let chatId = msg.chat.id;
+	let resp = match[1];
+	// axios.get('https://picsum.photos//500/500.jpg/?blur=3'){
+		
+	// }
+	let whitespaces = []
+	// console.log(resp)
+	for (i=0;i<resp.length;i++){
+		// console.log(i)
+		if (resp.charAt(i) == " "){
+			whitespaces.push(i);
+		}
+	}
+
+	const letter_inline = 20 
+	let offset = 1;
+	console.log(whitespaces)
+	for (i=0;i<whitespaces.length;i++){
+		if(whitespaces[i] > offset*letter_inline){
+			resp = resp.substring(0, whitespaces[i]+offset-1) + '\\n' + resp.substring(offset+whitespaces[i]);
+			offset += 1
+		}
+	}
+	let seed = Math.ceil(Math.random()*100000)
+
+	bot.sendPhoto(chatId,`https://i.pickadummy.com/500x500?text=${encodeURI(resp)}&f=Random&random_seed=${seed}&shadow=ffffff&color=000000`);
+
+});
+
+bot.on("polling_error", (err) => console.log(err));
